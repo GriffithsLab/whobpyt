0931c444ba7a5d7670b8b8b84bab18aae49945d0
John Griffiths <JohnGriffiths@users.noreply.github.com>
2022-05-24 15:20:58 -0400

    initial stuff for repo setup

---
+++ examples/eg001r__template.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+r"""
+=================================
+WhoBPyT Demo
+=================================
+
+
+"""  
+
+# sphinx_gallery_thumbnail_number = 1
+#
+# %%
+# Importage
+# --------------------------------------------------
+
+# KF Tools and related imports
+from whobpyt import *
+
+# Some neuroimaging library things
+import nibabel as nib
+from nilearn.plotting import plot_stat_map
+from nilearn.image import index_img
+
+# Some generic things
+from matplotlib import pyplot as plt
+import os
+import pandas as pd
+
+
+# %%
+# Some thing
+# --------------------------------------------------
+#
+# Say a thing
+# blah
+# blah blah
+#
+
+athing = '.'
+
+# %%
+# Do a thing
+# --------------------------------------------------
+
+print('A thing')
+
+
+# %%
+# Do another thing
+# ---------------------------------------------------
+#
+# The thing does a thing like so:
+# 1. Stuff
+# 2. More stuff
+#
+# And then more things
+
+print('Another thing')
+
+
---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt', 'r') as fp:
+    install_requires = fp.read().splitlines()
+install_requires = [i for i in install_requires if 'http' not in i]
+    
+    
+setup(
+    name="whobpyt", 
+    version="0.0",
+    author="KCNI Griffiths Lab et al.",
+    author_email="",
+    description="Whole-Brain Modelling in PyTorch",
+    keywords="Dynamics, Connectomics, Neuroimaging, fMRI, EEG, fNIRS, Machine Learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires = install_requires,
+    url='https://github.com/griffithslab/whobpyt',
+    license="BSD (3-clause)",
+    entry_points={},#{"console_scripts": ["eegnb=eegnb.cli.__main__:main"]},
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
+
