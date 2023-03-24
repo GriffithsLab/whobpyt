25335245ceed2afe61885a6607cd03bab526f8e2
John Wang <zhengwangdataanalyst@gmail.com>
2023-03-24 14:51:13 +0000

    use exponetial on gains

--- whobpyt/models/Robinson/robinson.py
+++ whobpyt/models/Robinson/robinson.py
@@ -31,19 +31,19 @@ class ParamsCT(AbstractParams):
             "alpha": [200/4, 0.], 
             "t0": [0.08, 0.],
             "g": [100, 0], 
-            "nu_ee ": [0.0528/1000, 0.], 
-            "nu_ii": [0.0528/1000, 0.],
-            "nu_ie": [0.02/1000, 0], 
-            "nu_es": [1.2/1000, 0], 
-            "nu_is": [1.2/1000, 0], 
-            "nu_se": [1.2/1000, 0], 
+            "nu_ee": [np.log(0.0528/1000), 0.], 
+            "nu_ii": [np.log(0.0528/1000), 0.],
+            "nu_ie": [np.log(0.02/1000), 0], 
+            "nu_es": [np.log(1.2/1000), 0], 
+            "nu_is": [np.log(1.2/1000), 0], 
+            "nu_se": [np.log(1.2/1000), 0], 
             "nu_si": [0.0, 0], 
-            "nu_ei": [0.4/1000, 0], 
-            "nu_sr": [0.01/1000, 0], 
+            "nu_ei": [np.log(0.4/1000), 0], 
+            "nu_sr": [np.log(0.01/1000), 0], 
             "nu_sn": [0.0, 0], 
-            "nu_re": [0.1/1000, 0], 
+            "nu_re": [np.log(0.1/1000), 0], 
             "nu_ri": [0.0, 0], 
-            "nu_rs": [0.1/1000, 0], 
+            "nu_rs": [np.log(0.1/1000), 0], 
             "nu_ss": [0.0, 0], 
             "nu_rr": [0.0, 0], 
             "nu_rn": [0.0, 0], 
@@ -103,7 +103,6 @@ class RNNROBINSON(AbstractNMM):
         """
         Parameters
         ----------
-
         tr : float
             tr of image
         step_size: float
@@ -206,7 +205,7 @@ def setModelParameters(model):
                 if var == 'lm':
                     size = getattr(model.param, var)[1].shape
                     setattr(model, var, Parameter(
-                        torch.tensor(getattr(model.param, var)[0] + 1 * np.ones((size[0], size[1])),
+                        torch.tensor(getattr(model.param, var)[0] -  np.ones((size[0], size[1])),
                                      dtype=torch.float32)))
                     param_reg.append(getattr(model, var))
                     # print(getattr(model, var))
@@ -304,7 +303,30 @@ def integration_forward(model, external, hx, hE):
     V_i_dot_window = []
     phi_i_window = []
     phi_i_dot_window = []
+    
 
+    alpha = model.alpha
+    beta = model.beta
+    alphaxbeta = model.alpha*model.beta
+    gamma = model.gamma
+    gamma_rs = model.gamma*1
+    nu_ee = torch.exp(model.nu_ee)
+    nu_ei = torch.exp(model.nu_ei)
+    nu_es = torch.exp(model.nu_es)
+    nu_ie = torch.exp(model.nu_ie)
+    nu_ii = torch.exp(model.nu_ii)
+    nu_is = torch.exp(model.nu_is)
+    nu_se = torch.exp(model.nu_se)
+    nu_si = model.nu_si
+    nu_ss = model.nu_ss
+    nu_sr = model.nu_sr
+    nu_sn = model.nu_sn
+    nu_re = torch.exp(model.nu_re)
+    nu_ri = model.nu_ri
+    nu_rs = torch.exp(model.nu_rs)
+    Q = model.Q_max
+    sig_theta = model.sig_theta
+    sigma = model.sigma
     # Use the forward model to get EEG signal at ith element in the window.
     for i_window in range(model.TRs_per_window):
 
@@ -328,28 +350,7 @@ def integration_forward(model, external, hx, hE):
 
             # LEd+torch.matmul(dg,E): Laplacian on delayed E
         
-            alpha = model.alpha
-            beta = model.beta
-            alphaxbeta = model.alpha*model.beta
-            gamma = model.gamma
-            gamma_rs = model.gamma*3
-            nu_ee = model.nu_ee
-            nu_ei = model.nu_ei
-            nu_es = model.nu_es
-            nu_ie = model.nu_ie
-            nu_ii = model.nu_ii
-            nu_is = model.nu_is
-            nu_se = model.nu_se
-            nu_si = model.nu_si
-            nu_ss = model.nu_ss
-            nu_sr = model.nu_sr
-            nu_sn = model.nu_sn
-            nu_re = model.nu_re
-            nu_ri = model.nu_ri
-            nu_rs = model.nu_rs
-            Q = model.Q_max
-            sig_theta = model.sig_theta
-            sigma = model.sigma
+            
             
             # Update the states by step-size.
             ddVe = V_e + dt * V_e_dot
@@ -362,30 +363,40 @@ def integration_forward(model, external, hx, hE):
             ddphir = phi_r + dt * phi_r_dot
 
             ones_mx = torch.ones((1,1)) # 1x1 ones matrix
-            noise_phi_e = torch.randn_like(phi_e) * 1 # noise for phi_e, stdev = 1, to be added to ddphiedot
-
-            ddVedot = V_e_dot + dt *  (-(1/alpha + 1/beta) * alphaxbeta * V_e_dot -alphaxbeta*V_e + \
-                                   alphaxbeta*(m(nu_ee) * phi_e - m(nu_ei) * phi_i + m(nu_es) * phi_s) +u_tms)
-            ddVidot = V_i_dot + dt * (-(1/alpha + 1/beta) * alphaxbeta * V_i_dot -alphaxbeta*V_i + \
-                                   alphaxbeta*(m(nu_ie) * phi_e + m(nu_ii) * phi_i + m(nu_is) * phi_s))
-            ddVsdot = V_s_dot + dt * (-(1/alpha + 1/beta) * alphaxbeta * V_s_dot - alphaxbeta * V_s + \
-                                   alphaxbeta*(m(nu_se) * torch.mean(phi_e, axis=0)*ones_mx + m(nu_si) * torch.mean(phi_i, axis=0)*ones_mx + \
+            noise_phi_e = torch.randn_like(phi_e) * 10 # noise for phi_e, stdev = 1, to be added to ddphiedot
+            noise_phi_i = torch.randn_like(phi_i) * 10
+            rVe = m(nu_ee) * phi_e - m(nu_ei) * phi_i + m(nu_es) * phi_s 
+            rVi = m(nu_ie) * phi_e + m(nu_ii) * phi_i + m(nu_is) * phi_s
+            rVs =torch.mean( m(nu_se) * phi_e, axis=0)*ones_mx +  torch.mean(m(nu_si) *phi_i, axis=0)*ones_mx + \
                                                m(nu_ss) * phi_s - m(nu_sr) * phi_r + m(nu_sn) * (0.025)*ones_mx + \
-                                               0.001 * torch.randn(1, 1)))
-            ddVrdot = V_r_dot + dt * (-(1/alpha + 1/beta) * alphaxbeta * V_r_dot -alphaxbeta*V_r + \
-                                   alphaxbeta*(m(nu_re) * torch.mean(phi_e, axis=0) * ones_mx + \
+                                               0.001 * torch.randn(1, 1)
+            rVr = m(nu_re) * torch.mean(phi_e, axis=0) * ones_mx + \
                                                m(nu_ri) * torch.mean(phi_i, axis=0) * ones_mx + \
-                                               m(nu_rs) * phi_s))
-            network_interactions = (lb * con_1 + m(model.g)) * LEd_l + \
-                                    1 * torch.matmul(dg_l, phi_e) # is implementation of global gain & SC 
+                                               m(nu_rs) * phi_s
+            network_interactions = (lb * con_1 + m(model.g)) * (LEd_l + \
+                                    1 * torch.matmul(dg_l, phi_e) )# is implementation of global gain & SC 
+            rphi_e = sigmoid(V_e, Q, sig_theta, sigma) + network_interactions + noise_phi_e +u_tms
+            rphi_i = sigmoid(V_i, Q, sig_theta, sigma) + noise_phi_i
+            rphi_s = sigmoid(V_s, Q, sig_theta, sigma)
+            rphi_r = sigmoid(V_r, Q, sig_theta, sigma)
+            
+            ddVedot = V_e_dot + dt *  (-(1/alpha + 1/beta) * alphaxbeta * V_e_dot -alphaxbeta*V_e + \
+                                   alphaxbeta*(rVe))
+            ddVidot = V_i_dot + dt * (-(1/alpha + 1/beta) * alphaxbeta * V_i_dot -alphaxbeta*V_i + \
+                                   alphaxbeta*(rVi))
+            ddVsdot = V_s_dot + dt * (-(1/alpha + 1/beta) * alphaxbeta * V_s_dot - alphaxbeta * V_s + \
+                                   alphaxbeta*(rVs))
+            ddVrdot = V_r_dot + dt * (-(1/alpha + 1/beta) * alphaxbeta * V_r_dot -alphaxbeta*V_r + \
+                                   alphaxbeta*(rVr))
+            
             ddphiedot = phi_e_dot + dt * (-2 * gamma * phi_e_dot - gamma**2 * phi_e + \
-                                       gamma**2 * (sigmoid(V_e, Q, sig_theta, sigma) + network_interactions + noise_phi_e))
+                                       gamma**2 * (rphi_e))
             ddphiidot = phi_i_dot + dt * (-2 * gamma_rs * phi_i_dot - gamma_rs**2 * phi_i + \
-                                       gamma_rs**2 * sigmoid(V_i, Q, sig_theta, sigma))
+                                       gamma_rs**2 * (rphi_i))
             ddphisdot = phi_s_dot + dt * (-2 * gamma_rs * phi_s_dot - gamma_rs**2 * phi_s + \
-                                       gamma_rs**2 * sigmoid(V_s, Q, sig_theta, sigma))
+                                       gamma_rs**2 * (rphi_s))
             ddphirdot = phi_r_dot + dt * (-2 * gamma_rs * phi_r_dot - gamma_rs**2 * phi_r + \
-                                       gamma_rs**2 * sigmoid(V_r, Q, sig_theta, sigma))
+                                       gamma_rs**2 * (rphi_r))
 
 
             # Calculate the saturation for model states (for stability and gradient calculation).
@@ -398,14 +409,14 @@ def integration_forward(model, external, hx, hE):
             phi_s = ddphis
             phi_r = ddphir
             
-            V_e_dot = ddVedot  # 1000*torch.tanh(ddEv/1000)#(con_1 + torch.tanh(df - con_1))
-            V_i_dot = ddVidot   # 1000*torch.tanh(ddIv/1000)#(con_1 + torch.tanh(dv - con_1))
-            V_s_dot = ddVsdot   # 1000*torch.tanh(ddMv/1000)#(con_1 + torch.tanh(dq - con_1))
-            V_r_dot = ddVrdot 
-            phi_e_dot = ddphiedot 
-            phi_i_dot = ddphiidot 
-            phi_s_dot = ddphisdot
-            phi_r_dot = ddphirdot
+            V_e_dot = 1000*torch.tanh(ddVedot/1000) # 1000*torch.tanh(ddEv/1000)#(con_1 + torch.tanh(df - con_1))
+            V_i_dot = 1000*torch.tanh(ddVidot/1000)   # 1000*torch.tanh(ddIv/1000)#(con_1 + torch.tanh(dv - con_1))
+            V_s_dot = 1000*torch.tanh(ddVsdot/1000)   # 1000*torch.tanh(ddMv/1000)#(con_1 + torch.tanh(dq - con_1))
+            V_r_dot = 1000*torch.tanh(ddVrdot/1000)
+            phi_e_dot = 1000*torch.tanh(ddphiedot/1000)
+            phi_i_dot = 1000*torch.tanh(ddphiidot/1000)
+            phi_s_dot = 1000*torch.tanh(ddphisdot/1000)
+            phi_r_dot = 1000*torch.tanh(ddphirdot/1000)
             
             # update placeholders for E buffer
             hE[:, 0] = phi_e[:, 0]
@@ -427,7 +438,7 @@ def integration_forward(model, external, hx, hE):
 
         model.lm_t = (lm_t - 1 / model.output_size * torch.matmul(torch.ones((1, model.output_size)),
                                                                   lm_t))  # s2o_coef *
-        temp = model.cy0 * torch.matmul(lm_t, phi_e[:model.node_size, :]) - 1 * model.y0
+        temp = model.cy0 * torch.matmul(model.lm_t, phi_e[:model.node_size, :]) - 1 * model.y0
         eeg_window.append(temp)  # torch.abs(E) - torch.abs(I) + 0.0*noiseEEG)
 
     # Update the current state.
