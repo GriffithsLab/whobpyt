c1308952da8675ddc1d80ee51a64a083ed226662
Sorenza Bastiaens <sorenza.bastiaens@gmail.com>
2023-06-30 10:59:16 -0400

    Updated validation JR

--- whobpyt/models/JansenRit/jansen_rit_validate.py
+++ whobpyt/models/JansenRit/jansen_rit_validate.py
@@ -16,11 +16,11 @@ class JansenRit_np():
         step_size=0.1
         self.step_size = step_size
         self.tr = tr  # tr ms (integration step 0.1 ms)
-        self.sc = sc  # scaling factor
+        self.sc = sc  # structural connectivity factor
         self.node_size = node_size  # num of ROI
         self.output_size = output_size  # num of EEG channels
         self.params = params
-        self.lm = lm  # length of the window
+        self.lm = lm  # leadfield matrix
         self.dist = dist  # distance between nodes
 
     def forward(self, external, hx, hE):
@@ -59,7 +59,7 @@ class JansenRit_np():
         sim_len = self.sim_len
         step_size = self.step_size
 
-        state_hist = np.zeros((int(sim_len/step_size), self.node_size, 6))
+        state_hist = np.zeros((int(sim_len/step_size), self.node_size, 7))
         M = init_state[:, 0:1]
         E = init_state[:, 1:2]
         I = init_state[:, 2:3]
@@ -69,6 +69,28 @@ class JansenRit_np():
 
         num_steps = int(sim_len/step_size)
         dt = step_size
+
+        # Update the Laplacian based on the updated connection gains w_bb.
+        w_b = np.exp(self.w_bb) * np.tensor(self.sc, dtype=np.float32)
+        w_n_b = w_b / np.linalg.norm(w_b)
+        self.sc_m_b = w_n_b
+        dg_b = -np.diag(np.sum(w_n_b, dim=1))
+
+        # Update the Laplacian based on the updated connection gains w_ff.
+        w_f = np.exp(self.w_ff) * np.tensor(self.sc, dtype=np.float32)
+        w_n_f = w_f / np.linalg.norm(w_f)
+        self.sc_m_f = w_n_f
+        dg_f = -np.diag(np.sum(w_n_f, dim=1))
+
+        # Update the Laplacian based on the updated connection gains w_ll.
+        w_l = np.exp(self.w_ll) * np.tensor(self.sc, dtype=np.float32)
+        w_n_l = (0.5 * (w_l + np.transpose(w_l, 0, 1))) / np.linalg.norm(
+            0.5 * (w_l + np.transpose(w_l, 0, 1)))
+        self.sc_fitted = w_n_l
+        dg_l = -np.diag(np.sum(w_n_l, dim=1))
+
+
+
         self.delays = (self.dist / self.mu).astype(int)
 
         # TODO currently single node, need to add all the connections and make it multiple nodes
@@ -90,9 +112,9 @@ class JansenRit_np():
             LEd_f = np.reshape(np.sum(self.w_n_f * np.transpose(Ed, 0, 1), 1), (self.node_size, 1))
             LEd_l = np.reshape(np.sum(self.w_n_l * np.transpose(Ed, 0, 1), 1), (self.node_size, 1))
 
-            rM = k * ki * u_tms + std_in*np.random.randn(self.node_size, 1) + g * LEd_l + 1  
-            rE = std_in*np.random.randn(self.node_size, 1) + g_f * LEd_f + 1
-            rI = std_in*np.random.randn(self.node_size, 1) + g_b * (-LEd_b - 1)
+            rM = k * ki * u_tms + std_in*np.random.randn(self.node_size, 1) + g * (LEd_l + 1 * np.matmul(dg_l, M)) 
+            rE = std_in*np.random.randn(self.node_size, 1) + g_f * (LEd_f + 1 * np.matmul(dg_f, E - I))
+            rI = std_in*np.random.randn(self.node_size, 1) + g_b * (-LEd_b - 1 * np.matmul(dg_b, E - I))
 
             dM = dt * Mv
             dE = dt * Ev
@@ -108,6 +130,8 @@ class JansenRit_np():
             Mv = Mv + dMv
             Ev = Ev + dEv
             Iv = Iv + dIv
+            hE = np.cat([M, hE[:, :-1]], dim=1)  # update placeholders for pyramidal buffer
+
 
             state_hist[i, :, 0:1] = M
             state_hist[i, :, 1:2] = E
@@ -116,6 +140,13 @@ class JansenRit_np():
             state_hist[i, :, 4:5] = Ev
             state_hist[i, :, 5:6] = Iv
 
-            return state_hist
+            # Capture the states at every step .
+            lm_t = (self.lm.T / np.sqrt(self.lm ** 2).sum(1)).T
+            self.lm_t = (lm_t - 1 / self.output_size * np.matmul(np.ones((1, self.output_size)), lm_t))
+            temp = cy0 * np.matmul(self.lm_t, M[:200, :]) - 1 * y0
+            state_hist[i, :, 6:7] = temp # eeg_window
+
+            # Should then downsample the state_hist to the sampling rate of the EEG
+        return state_hist, hE
 
     
