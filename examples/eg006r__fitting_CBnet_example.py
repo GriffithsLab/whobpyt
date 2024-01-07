4ab1706437ae3da460fd0e2a8f6d03ba6381fb1f
John Wang <zhengwangdataanalyst@gmail.com>
2024-01-07 21:22:49 +0000

    import dataloader again

--- examples/eg006r__fitting_CBnet_example.py
+++ examples/eg006r__fitting_CBnet_example.py
@@ -22,7 +22,8 @@ sys.path.append('..')
 
 # whobpyt stuff
 import whobpyt
-from whobpyt.datatypes import par, dataloader, Recording
+from whobpyt.datatypes import par, Recording
+from whobpyt.data import dataloader
 from whobpyt.models.JansenRit import RNNJANSEN, ParamsJR
 from whobpyt.optimization.custom_cost_JR import CostsJR
 from whobpyt.run import Model_fitting
