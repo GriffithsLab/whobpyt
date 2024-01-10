965003ee8e17e258af262a00d09613c5c04a6727
sbastiaens <62792658+sbastiaens@users.noreply.github.com>
2024-01-10 16:02:12 -0500

    Update jansen_rit_validate.py

--- whobpyt/models/JansenRit/jansen_rit_validate.py
+++ whobpyt/models/JansenRit/jansen_rit_validate.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class JansenRit_np():
 
-     def __init__(self, node_size, step_size, output_size, tr, sc, lm, dist, params):      
+    def __init__(self, node_size, step_size, output_size, tr, sc, lm, dist, params):      
 
         
         # Initialize the JR Model 
@@ -13,16 +13,24 @@ class JansenRit_np():
         #  num_regions: Int - Number of nodes in network to model
         #  params: Params_JR - The parameters that all nodes in the network will share
         #  Con_Mtx: Tensor [num_regions, num_regions] - With connectivity (eg. structural connectivity)
-        #step_size=0.1
+        # step_size=0.1
         self.step_size = step_size
+        self.tr = tr  # tr ms (integration step 0.1 ms)
         self.sc = sc  # structural connectivity factor
         self.node_size = node_size  # num of ROI
         self.output_size = output_size  # num of EEG channels
         self.params = params
         self.lm = lm  # leadfield matrix
         self.dist = dist  # distance between nodes
+        self.state_size = 6  #
 
-    def forward(self, external, hx, hE):
+    def createIC(self, ver):
+        state_lb = -0.5
+        state_ub = 0.5
+        
+        return np.random.uniform(state_lb, state_ub, (self.node_size, self.state_size))	
+
+    def forward(self, external, hx, hE, sim_len):
 
         # Runs the JR model
 
@@ -49,14 +57,13 @@ class JansenRit_np():
         g_f = self.params.g_f.npValue()
         g_b = self.params.g_b.npValue()
 
-        next_state = {}
-        
+
         # Sigmoid function
         def sigmoid(x, vmax, v0, r):
             return vmax / (1 + np.exp(r * (v0 - x)))
         
         init_state = hx
-        sim_len = self.sim_len
+        sim_len = sim_len
         step_size = self.step_size
 
         state_hist = np.zeros((int(sim_len/step_size), self.node_size, 7))
@@ -69,29 +76,31 @@ class JansenRit_np():
 
         num_steps = int(sim_len/step_size)
         dt = step_size
-
+        self.w_bb = np.zeros((self.node_size, self.node_size))
+        self.w_ff = np.zeros((self.node_size, self.node_size))
+        self.w_ll = np.zeros((self.node_size, self.node_size))
         # Update the Laplacian based on the updated connection gains w_bb.
-        w_b = np.exp(self.w_bb) * np.tensor(self.sc, dtype=np.float32)
+        w_b = np.exp(self.w_bb) * np.array(self.sc)
         w_n_b = w_b / np.linalg.norm(w_b)
         self.sc_m_b = w_n_b
-        dg_b = -np.diag(np.sum(w_n_b, dim=1))
+        dg_b = -np.diag(np.sum(w_n_b, axis=1))
 
         # Update the Laplacian based on the updated connection gains w_ff.
-        w_f = np.exp(self.w_ff) * np.tensor(self.sc, dtype=np.float32)
+        w_f = np.exp(self.w_ff) * np.array(self.sc)
         w_n_f = w_f / np.linalg.norm(w_f)
         self.sc_m_f = w_n_f
-        dg_f = -np.diag(np.sum(w_n_f, dim=1))
+        dg_f = -np.diag(np.sum(w_n_f, axis=1))
 
         # Update the Laplacian based on the updated connection gains w_ll.
-        w_l = np.exp(self.w_ll) * np.tensor(self.sc, dtype=np.float32)
-        w_n_l = (0.5 * (w_l + np.transpose(w_l, 0, 1))) / np.linalg.norm(
-            0.5 * (w_l + np.transpose(w_l, 0, 1)))
+        w_l = np.exp(self.w_ll) * np.array(self.sc)
+        w_n_l = (0.5 * (w_l + np.transpose(w_l, (1, 0)))) / np.linalg.norm(
+            0.5 * (w_l + np.transpose(w_l, (1, 0))))
         self.sc_fitted = w_n_l
-        dg_l = -np.diag(np.sum(w_n_l, dim=1))
+        dg_l = -np.diag(np.sum(w_n_l, axis=1))
 
 
 
-        self.delays = (self.dist / self.mu).astype(int)
+        self.delays = (self.dist / mu).astype(int)
 
         # TODO currently single node, need to add all the connections and make it multiple nodes
         for i in range(num_steps):
@@ -107,11 +116,11 @@ class JansenRit_np():
             # Backward is P-I
             Ed = np.zeros((self.node_size, self.node_size))
             hE_new = hE.copy()
-            Ed = hE_new.gather(1,self.delays)
-            LEd_b = np.reshape(np.sum(self.w_n_b * np.transpose(Ed, 0, 1), 1), (self.node_size, 1)) # Not sure if this needs to be included in validation
-            LEd_f = np.reshape(np.sum(self.w_n_f * np.transpose(Ed, 0, 1), 1), (self.node_size, 1))
-            LEd_l = np.reshape(np.sum(self.w_n_l * np.transpose(Ed, 0, 1), 1), (self.node_size, 1))
-
+            Ed = hE_new[1,self.delays] #hE_new.gather(1,self.delays)
+            LEd_b = np.reshape(np.sum(w_n_b * np.transpose(Ed, (1, 0)), 1), (self.node_size, 1)) # Not sure if this needs to be included in validation
+            LEd_f = np.reshape(np.sum(w_n_f * np.transpose(Ed, (1, 0)), 1), (self.node_size, 1))
+            LEd_l = np.reshape(np.sum(w_n_l * np.transpose(Ed, (1, 0)), 1), (self.node_size, 1))
+            u_tms = 200 # Need to only had within a certain time frame, test again with 0 
             rM = k * ki * u_tms + std_in*np.random.randn(self.node_size, 1) + g * (LEd_l + 1 * np.matmul(dg_l, M)) 
             rE = std_in*np.random.randn(self.node_size, 1) + g_f * (LEd_f + 1 * np.matmul(dg_f, E - I))
             rI = std_in*np.random.randn(self.node_size, 1) + g_b * (-LEd_b - 1 * np.matmul(dg_b, E - I))
@@ -130,8 +139,7 @@ class JansenRit_np():
             Mv = Mv + dMv
             Ev = Ev + dEv
             Iv = Iv + dIv
-            hE = np.cat([M, hE[:, :-1]], dim=1)  # update placeholders for pyramidal buffer
-
+            hE = np.concatenate((M, hE[:, :-1]), axis=1) #np.cat([M, hE[:, :-1]], axis=1)  # update placeholders for pyramidal buffer
 
             state_hist[i, :, 0:1] = M
             state_hist[i, :, 1:2] = E
@@ -141,19 +149,12 @@ class JansenRit_np():
             state_hist[i, :, 5:6] = Iv
 
             # Capture the states at every step .
-            lm_t = (self.lm.T / np.sqrt(self.lm ** 2).sum(1)).T
-            self.lm_t = (lm_t - 1 / self.output_size * np.matmul(np.ones((1, self.output_size)), lm_t))
-            temp = cy0 * np.matmul(self.lm_t, M[:200, :]) - 1 * y0
-            state_hist[i, :, 6:7] = temp # eeg_window
+            #lm_t = (self.lm.T / np.sqrt(self.lm ** 2).sum(1)).T
+            #self.lm_t = (lm_t - 1 / self.output_size * np.matmul(np.ones((1, self.output_size)), lm_t))
+            #temp = cy0 * np.matmul(self.lm_t, M[:self.node_size, :]) - 1 * y0
+            #state_hist[i, :, 6:7] = temp # eeg_window
 
-        next_state['M'] = state_hist[:,:,0]
-        next_state['E'] = state_hist[:,:,1]
-        next_state['I'] = state_hist[:,:,2]
-        next_state['Mv'] = state_hist[:,:,3]
-        next_state['Ev'] = state_hist[:,:,4]
-        next_state['Iv'] = state_hist[:,:,5]
-        next_state['eeg'] = state_hist[:,:,6]
             # Should then downsample the state_hist to the sampling rate of the EEG
-        return next_state, hE
+        return state_hist, hE
 
     
