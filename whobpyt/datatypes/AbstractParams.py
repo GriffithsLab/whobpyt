d6cd917f0369b75c0a864fc703c9782c1faf9511
sbastiaens <62792658+sbastiaens@users.noreply.github.com>
2024-03-13 16:54:59 -0400

    Update AbstractParams.py

--- whobpyt/datatypes/AbstractParams.py
+++ whobpyt/datatypes/AbstractParams.py
@@ -2,14 +2,29 @@ import torch
 from whobpyt.datatypes.parameter import par
 
 class AbstractParams:
-    # This class stores the parameters used by a model. The parameters may be for the Neural Mass Model and/or Neuroimaging Modality.
-    # It should be useable by both the pytorch model for training and a numpy model for parameter verification. 
-
+    '''
+    This class stores the parameters used by a model. The parameters may be for the Neural Mass Model and/or Neuroimaging Modality. 
+    It should be useable by both the pytorch model for training and a numpy model for parameter verification. 
+    '''
     def __init__(self, **kwargs):
-        # Define the parameters using the par data structure
+        '''
+        Initialize the AbstractParams object and define the parameters using the par data structure
+        
+        Args:
+            **kwargs: Keyword arguments for the model parameters.
+
+        Returns:
+            None
+        '''
         pass
     
     def getFittedNames(self):
+        '''
+        Function to obtain the named list of parameters being fitted
+
+        Returns:
+            Named list of parameters that are being fitted
+        '''
         # Returns a named list of paramters that are being fitted
         # Assumes the par datastructure is being used for parameters
         
@@ -26,6 +41,15 @@ class AbstractParams:
         return fp
         
     def to(self, device):
+        '''
+        Moves all parameters between CPU and GPU
+        
+        Args:
+            device: torch.device object defining wether CPU or GPU
+
+        Returns:
+            None
+        '''
         # Moves all parameters between CPU and GPU
         
         vars_names = [a for a in dir(self) if not a.startswith('__')]
