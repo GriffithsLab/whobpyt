5b2a194dc5e9df6e2c7db92e44804cf349fcb529
aldensyi <sminyi0130@gmail.com>
2024-06-20 21:21:31 -0500

    First Draft Push; The code ran from start to finish without an issue.

--- whobpyt/models/wong_wang/wong_wang.py
+++ whobpyt/models/wong_wang/wong_wang.py
@@ -4,8 +4,8 @@ Neural Mass Model fitting module for Wong-Wang model
 """
 
 import torch
-from torch.nn.parameter import Parameter
-from whobpyt.datatypes import AbstractNeuralModel, AbstractParams, Parameter
+from torch.nn.parameter import Parameter as ptParameter
+from whobpyt.datatypes import AbstractNeuralModel, AbstractParams, Parameter as par
 from whobpyt.functions.arg_type_check import method_arg_type_check
 import numpy as np # for numerical operations
 
@@ -194,7 +194,7 @@ class ReducedWongWangModel(AbstractNeuralModel):
         # Set w_bb, w_ff, and w_ll as attributes as type Parameter if use_fit_gains is True
         if self.use_fit_gains:
             
-            self.w_ll = Parameter(torch.tensor(np.zeros((self.node_size, self.node_size)) + 0.05, # the lateral gains
+            self.w_ll = ptParameter(torch.tensor(np.zeros((self.node_size, self.node_size)) + 0.05, # the lateral gains
                                                 dtype=torch.float32))
             self.params_fitted['modelparameter'].append(self.w_ll)
         else:
@@ -409,48 +409,48 @@ class ReducedWongWangParams(AbstractParams):
         
         param = {
 
-            "std_in": Parameter(0.02),  # standard deviation of the Gaussian noise
-            "std_out": Parameter(0.02),  # standard deviation of the Gaussian noise
+            "std_in": par(0.02),  # standard deviation of the Gaussian noise
+            "std_out": par(0.02),  # standard deviation of the Gaussian noise
             # Parameters for the ODEs
             # Excitatory population
-            "W_E": Parameter(1.),  # scale of the external input
-            "tau_E": Parameter(100.),  # decay time
-            "gamma_E": Parameter(0.641 / 1000.),  # other dynamic parameter (?)
+            "W_E": par(1.),  # scale of the external input
+            "tau_E": par(100.),  # decay time
+            "gamma_E": par(0.641 / 1000.),  # other dynamic parameter (?)
 
             # Inhibitory population
-            "W_I": Parameter(0.7),  # scale of the external input
-            "tau_I": Parameter(10.),  # decay time
-            "gamma_I": Parameter(1. / 1000.),  # other dynamic parameter (?)
+            "W_I": par(0.7),  # scale of the external input
+            "tau_I": par(10.),  # decay time
+            "gamma_I": par(1. / 1000.),  # other dynamic parameter (?)
 
             # External input
-            "I_0": Parameter(0.32),  # external input
-            "I_external": Parameter(0.),  # external stimulation
+            "I_0": par(0.32),  # external input
+            "I_external": par(0.),  # external stimulation
 
             # Coupling parameters
-            "g": Parameter(20.),  # global coupling (from all nodes E_j to single node E_i)
-            "g_EE": Parameter(.1),  # local self excitatory feedback (from E_i to E_i)
-            "g_IE": Parameter(.1),  # local inhibitory coupling (from I_i to E_i)
-            "g_EI": Parameter(0.1),  # local excitatory coupling (from E_i to I_i)
+            "g": par(20.),  # global coupling (from all nodes E_j to single node E_i)
+            "g_EE": par(.1),  # local self excitatory feedback (from E_i to E_i)
+            "g_IE": par(.1),  # local inhibitory coupling (from I_i to E_i)
+            "g_EI": par(0.1),  # local excitatory coupling (from E_i to I_i)
 
-            "aE": Parameter(310),
-            "bE": Parameter(125),
-            "dE": Parameter(0.16),
-            "aI": Parameter(615),
-            "bI": Parameter(177),
-            "dI": Parameter(0.087),
+            "aE": par(310),
+            "bE": par(125),
+            "dE": par(0.16),
+            "aI": par(615),
+            "bI": par(177),
+            "dI": par(0.087),
 
             # Output (BOLD signal)
-            "alpha": Parameter(0.32),
-            "rho": Parameter(0.34),
-            "k1": Parameter(2.38),
-            "k2": Parameter(2.0),
-            "k3": Parameter(0.48),  # adjust this number from 0.48 for BOLD fluctruate around zero
-            "V": Parameter(.02),
-            "E0": Parameter(0.34),
-            "tau_s": Parameter(1 / 0.65),
-            "tau_f": Parameter(1 / 0.41),
-            "tau_0": Parameter(0.98),
-            "mu": Parameter(0.5)
+            "alpha": par(0.32),
+            "rho": par(0.34),
+            "k1": par(2.38),
+            "k2": par(2.0),
+            "k3": par(0.48),  # adjust this number from 0.48 for BOLD fluctruate around zero
+            "V": par(.02),
+            "E0": par(0.34),
+            "tau_s": par(1 / 0.65),
+            "tau_f": par(1 / 0.41),
+            "tau_0": par(0.98),
+            "mu": par(0.5)
 
         }
 
