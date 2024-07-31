4b5e8c6a190ca0c2915b2e264088aa680a8b24d4
sbastiaens <sorenza.bastiaens@gmail.com>
2024-07-31 14:41:22 -0400

    Folder and files for robinson

---
+++ whobpyt/models/robinson/robinson_lin.py
@@ -0,0 +1,20 @@
+"""
+
+WhoBPyt Robinson Linear model classes
+--------------------------------------
+
+Authors: Zheng Wang, John Griffiths, Davide Momi, Sorenza Bastiaens, Parsa Oveisi, Kevin Kadak, Shreyas Harita, Minarose Ismail
+
+Neural Mass Model fitting module for Linear Robinson for M/EEG
+
+"""
+
+
+
+
+"""
+Importage
+---------
+"""
+
+
