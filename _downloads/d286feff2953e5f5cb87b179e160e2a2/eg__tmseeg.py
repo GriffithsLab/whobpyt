2610b421e26342c4e5c45c093fe243891d1cbe34
JohnGriffiths <JohnGriffiths@users.noreply.github.com>
2025-04-28 21:13:05 +0000

    deploy: b83d65559632fd82d9d24e337e681492a34e285a

--- _downloads/d286feff2953e5f5cb87b179e160e2a2/eg__tmseeg.py
+++ _downloads/d286feff2953e5f5cb87b179e160e2a2/eg__tmseeg.py
@@ -57,7 +57,7 @@ evoked = epoched.average()
 
 # %%
 # Load Atlas
-file_name = os.path.join(data_dir, 'Schaefer2018_200Parcels_7Networks_order_FSLMNI152_2mm.Centroid_RAS.csv')
+file_name = os.path.join(data_dir, 'Schaefer2018_200Parcels_7Networks_order_FSLMNI152_2mm.Centroid_RAS.txt')
 atlas = pd.read_csv(file_name)
 labels = atlas['ROI Name']
 coords = np.array([atlas['R'], atlas['A'], atlas['S']]).T
