4728596322363940c10e3acb3da5bd1075ac3be4
John Wang <zhengwangdataanalyst@gmail.com>
2025-09-15 18:34:54 +0000

    update init

--- whobpyt/run/__init__.py
+++ whobpyt/run/__init__.py
@@ -1,4 +1,4 @@
 from .model_fitting import ModelFitting
 from .custom_fitting import FittingFNGFPG
 from .batch_fitting import FittingBatch
-from .Model_fitting_fq import Model_fitting_fq
+from .model_fitting_fq import Model_fitting_fq
